e5baed8b81de397fdc842d3e3975e5ccad1f98e1
Masahiro913 <gonzaes0819@yahoo.co.jp>
2020-11-19 11:22:20 +0900

    Initial commit

---
+++ iris.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+
+iris_data = pd.read_csv("tableconvert_csv_rz1wsc.csv", encoding="utf-8")
+
+
+y = iris_data.loc[:,"species"]
+x = iris_data.loc[:,["sepal_length", "sepal_width", "petail_length", "petal_width"]]
+x.drop(x.columns[np.isnan(x).any()], axis=1)
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2,train_size=0.8,shuffle = True)
+
+
+clf = SVC()
+clf.fit(x_train, y_train)
+
+
+y_pred = clf.prediction(x_test)
+print("accurate = ", accuracy_score(y_test,y_pred))
