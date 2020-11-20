4b11d8bed9d9188501037c048e84a33ec6e48cda
Masahiro913 <gonzaes0819@yahoo.co.jp>
2020-11-21 00:11:07 +0900

    修正

--- iris.py
+++ iris.py
@@ -9,8 +9,8 @@ iris_data = pd.read_csv("tableconvert_csv_rz1wsc.csv", encoding="utf-8")
 
 
 y = iris_data.loc[:,"species"]
-x = iris_data.loc[:,["sepal_length", "sepal_width", "petail_length", "petal_width"]]
-x.drop(x.columns[np.isnan(x).any()], axis=1)
+x = iris_data.loc[:,["sepal_length", "sepal_width", "petal_length", "petal_width"]]
+
 
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2,train_size=0.8,shuffle = True)
 
@@ -19,5 +19,5 @@ clf = SVC()
 clf.fit(x_train, y_train)
 
 
-y_pred = clf.prediction(x_test)
+y_pred = clf.predict(x_test)
 print("accurate = ", accuracy_score(y_test,y_pred))
